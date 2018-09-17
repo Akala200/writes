f5b99da0550a5184b81146adb9d5f003ca9eba7f
emmanuel <emmanuelukwuegbu2016@gmail.com>
2018-09-17 04:39:48 -0700

    changed

--- easywriter/settings/base.py
+++ easywriter/settings/base.py
@@ -111,10 +111,9 @@ DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( os.path.join('static'), )
+#STATICFILES_DIRS = ( os.path.join('static'), )
 
-
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(DIR, 'static')
  
 
 
