15bef607cb9c8c295e482bd55bca133d7eefa466
emmanuel <emmanuelukwuegbu2016@gmail.com>
2018-10-02 05:45:27 -0700

    done

--- easywriter/settings/base.py
+++ easywriter/settings/base.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 
-DEBUG = False
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'essay---kitten.herokuapp.com', 'www.essaykitten.com']
 
